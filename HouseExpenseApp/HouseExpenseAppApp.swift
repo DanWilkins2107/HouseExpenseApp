4c97b40fdecd8d712a83c56fd94a2358fe811579
Dan Wilkins <danwilkins2003@gmail.com>
2023-10-04 16:50:13 +0100

    Initial Commit

---
+++ HouseExpenseApp/HouseExpenseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseExpenseAppApp.swift
+//  HouseExpenseApp
+//
+//  Created by Daniel Wilkins on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct HouseExpenseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
