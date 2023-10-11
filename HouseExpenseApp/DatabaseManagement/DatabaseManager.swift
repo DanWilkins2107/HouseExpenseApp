129b9eece8248b765d8240c44423b425db49b5db
Dan Wilkins <danwilkins2003@gmail.com>
2023-10-11 17:26:50 +0100

    Made changes

---
+++ HouseExpenseApp/DatabaseManagement/DatabaseManager.swift
@@ -0,0 +1,13 @@
+//
+//  DatabaseManager.swift
+//  HouseExpenseApp
+//
+//  Created by Daniel Wilkins on 11/10/2023.
+//
+
+import Foundation
+import Supabase
+
+class DatabaseManager: ObservableObject {
+    let client = SupabaseClient(supabaseURL: <#T##URL#>, supabaseKey: <#T##String#>)
+}
