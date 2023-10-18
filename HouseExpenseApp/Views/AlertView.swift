eeb23a9c56b6316535da639be98f8087b9f6e839
Dan Wilkins <danwilkins2003@gmail.com>
2023-10-18 17:56:08 +0100

    Sign up done for now

---
+++ HouseExpenseApp/Views/AlertView.swift
@@ -0,0 +1,21 @@
+//
+//  AlertViews.swift
+//  HouseExpenseApp
+//
+//  Created by Daniel Wilkins on 17/10/2023.
+//
+
+import SwiftUI
+
+struct AlertView: View {
+    var foregroundColor: Color
+    var text: String
+    var body: some View {
+        VStack {
+            ZStack {
+                Rectangle(.green)
+            }
+        }
+        .padding
+    }
+}
